dc15b2c2d02fef5ff352cb61e26bda38a570cfb4
Adithya N <adithya.cs18@bmsce.ac.in>
2019-07-22 18:06:31 +0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="bg-gray-700">
+    <main-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainView from "./components/MainView.vue";
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
+  name: 'app',
+  components: { MainView },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
   }
-};
+}
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import './assets/sass/index.sass'
 </style>
